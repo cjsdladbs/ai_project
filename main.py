5243854fd2d112f854ab54e7e0ee63af5d4a540a
cjsdladbs <cjsdladbs@gmail.com>
2026-05-18 10:01:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title('로제마라떡볶')
+st.title('로제마라떡볶이')
 a=st.text_input('이름.')
 b=st.selectbox('좋아하는 애니를 선택하세요',['그비돌','나히아','향꽃늠피','귀칼','톱맨','주술회전'])
 if st.button('북딱 흔드르라'):
