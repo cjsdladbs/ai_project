0617e616dd371490f28e7db893e57cacda1021b2
cjsdladbs <cjsdladbs@gmail.com>
2026-05-18 10:00:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-st.title('나의 첫 웹서비스 만들기')
-st.text_input('키미노 나마에와')
-st.selectbox('좋아하는 애니를 선택하세요',['그비돌','나히아','향꽃늠피','귀칼','톱맨','주술회전'])
-st.button('북딱 흔드르라')
+st.title('로제마라떡볶')
+a=st.text_input('이름.')
+b=st.selectbox('좋아하는 애니를 선택하세요',['그비돌','나히아','향꽃늠피','귀칼','톱맨','주술회전'])
+if st.button('북딱 흔드르라'):
+  st.write(a+'ㅎㅇ')
